e36276ecfb16adbe25bf645a46ae946864f82ad1
Matt Pileggi <mpileggi@k12.com>
2012-12-05 21:26:56 -0500

    working on speed library

---
+++ speedlib.js
@@ -0,0 +1,63 @@
+(function() {
+  var core = {};
+  var $outer,$doc = document,$win = window;
+  
+  var createElement = function(tagname,opts) {
+    var ele = $doc.createElement(tagname);
+    for (var key in opts) {
+      if (opts.hasOwnProperty(key)) {
+        switch(key) {
+          case 'html':
+            ele.innerHTML = opts[key];
+            break;
+          default:
+            ele.setAttribute(key,opts[key]);
+        }
+      }
+    }
+    return ele
+  };
+  
+  // cache the result so we don't have to inspect every single time
+  var binder = (function() {
+    return Element.prototype.addEventListener || Element.prototype.attachEvent;
+  })();
+  
+  /**
+   *  Bind an event to an element
+   *  @param{Element} ele the Element to be bound to an event
+   *  @param{String} eventName the name of the event to bind
+   *  @param{function} handler the event handler function
+   **/
+  var bind = function(ele,eventName,handler) {
+    console.log(binder);
+    binder.call(ele,eventName,handler);
+  };
+  
+  /**
+   *  Event delegation using the Element as the root and matches any
+   *  children
+   **/
+  var delegate = function(eventName,selectorString,callback) {
+  }
+  
+  core.init = function() {
+    var frag = $doc.createDocumentFragment();
+    var div = createElement('div',{id:'outer'});
+    var a = createElement('a',{'className':'myclass',html:'Test'})
+    var p = createElement('p',{'id':'p'});
+    p.appendChild(a);
+    div.appendChild(p);
+    frag.appendChild(div);
+    $doc.body.appendChild(frag);
+    $outer = $doc.getElementById('outer');
+    bind($outer,'click',function(e) {
+      e = e || $win.event;
+      var target = e.target || e.srcElement;
+      
+      console.log(target.tagName);
+    });;
+  }
+  
+  window.$peed = core;
+})();

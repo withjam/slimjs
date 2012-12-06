236f936e98ac1f93bcb3d5e0bcecac2b82c5c4b7
Matt Pileggi <mpileggi@k12.com>
2012-12-06 00:40:01 -0500

    speeding up delegate

--- speedlib.js
+++ speedlib.js
@@ -30,35 +30,19 @@
    *  children
    **/
   var delegate = function(rootEle,eventName,selectorString,callback) {
+    console.log(rootEle);
     bind(rootEle,eventName,function(e) {
       var matches = Array.prototype.slice.call(rootEle.querySelectorAll(selectorString));
       e = e || window.event;
       var target = e.target || e.srcElement;
-      do {
-        if (matches.indexOf(target)) {
-          console.log('true');
-          callback(target);
-        } else {
-          console.log('false');
-        }
-        target = target.parentNode;
-      } while (target !== rootEle)
     });
   }
   
   var core = {
     init: function() {
-      var frag = $doc.createDocumentFragment();
-      var div = createElement('div',{id:'outer'});
-      var a = createElement('a',{'className':'myclass',html:'Test'})
-      var p = createElement('p',{'id':'p'});
-      p.appendChild(a);
-      div.appendChild(p);
-      frag.appendChild(div);
-      $doc.body.appendChild(frag);
-      $outer = $doc.getElementById('outer');
-      delegate($outer,'click','a',function(target) {
-        console.log(target.tagName);
+      $outer = document.getElementById('outer');
+      delegate($outer,'click','.item',function(target) {
+        console.log(target.className);
       });;
     },
     bind: bind,

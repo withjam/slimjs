c2edc7982574bb481543405a8ea2addf8c0b6167
Matt Pileggi <mpileggi@k12.com>
2012-12-06 12:24:58 -0500

    tests will update dom html

--- tests.js
+++ tests.js
@@ -1,12 +1,18 @@
+var clicked = function(e) {
+  e = e || window.event;
+  var t = e.actor || e.target || e.srcElement;
+  t.firstElementChild.firstElementChild.innerHTML = 'Clicked Upon';
+}
+
 var del = document.getElementById('delegate');
-$lim.event.delegate(del,'click','.item',function() {});
-var cli = document.getElementById('single');
-$lim.event.bind(cli,'click',function() {});
+$lim.event.delegate(del,'click','.item',clicked);
+var cli = document.querySelector('#single');
+$lim.event.bind(cli,'click',clicked);
 
 JSLitmus.test("Delegate bind",function() {
-  eventFire(document.querySelector('#delegate .item a'),'click');
+  eventFire(document.querySelector('#delegate .item a span'),'click');
 });
 
 JSLitmus.test("Single bind",function() {
-  eventFire(document.querySelector('#single a'),'click');
+  eventFire(document.querySelector('#single'),'click');
 });

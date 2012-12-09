200544d7656037b08e567109ad42948bfcf01e03
Matt Pileggi <mpileggi@k12.com>
2012-12-08 19:31:19 -0500

    trim

--- slim.js
+++ slim.js
@@ -11,6 +11,43 @@
     }
   };
   
+  $lim.util = {};
+  
+  // custom trim function that will be used if a faster native trim is not available
+  var _trim = function(str) {
+      var c;
+      for (var i = 0; i < str.length; i++) {
+        c = str.charCodeAt(i);
+        if (c < 8192) {
+          if (c < 256 && (c == 32 || (c >= 9 && c <= 13) || c == 160)) continue;
+          else if (c == 5760 || c == 6158) continue;
+          break;
+        } else if (c <= 8202 || c == 8232 || c == 8233 || c == 8239 || c == 8287 || c == 12288) continue;
+        break;
+      }
+      for (var j = str.length - 1; j >= i; j--) {
+        c = str.charCodeAt(j);
+        if (c < 8192) {
+          if (c < 256 && (c == 32 || (c >= 9 && c <= 13) || c == 160)) continue;
+          else if (c == 5760 || c == 6158) continue;
+          break;
+        } else if (c <= 8202 || c == 8232 || c == 8233 || c == 8239 || c == 8287 || c == 12288) continue;
+        break;
+      }
+      return str.substring(i, j + 1);
+      };
+  /**
+    * Trim function idea inspired by High Powered JavaScript.  Used based on tests from http://jsperf.com/mega-trim-test/22
+    * @param {string} str the string to trim
+    * @return {string} the trimmed version of the string
+  **/
+  if (!String.prototype.trim || "\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF".trim() || navigator.userAgent.toString().toLowerCase().indexOf("chrome") != -1) {
+    $lim.util.trim = _trim;
+  } else {
+    $lim.util.trim = function(str) { return str.trim(); };
+  }; // use native trim when available except on chrome - Consider replacing String.prototype.trim since the wrapper function is slightly slower than using the prototype function (I currently hesitate to modify the prototype of a base object)
+
+  
   $lim.dom = {};
           
   /**
